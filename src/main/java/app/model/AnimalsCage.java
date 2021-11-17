ef7b4843dd15a615b677289e2312a7eae992e4d6
Helya <z>
2021-11-17 16:53:07 +0300

    Initial commit

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,30 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@Component
+public class AnimalsCage {
+
+    private Animal animal;
+    private Timer timer;
+
+    @Autowired
+    public AnimalsCage(@Qualifier("dog") Animal animal,
+                       @Qualifier("timer") Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(new Timer().getTime());
+        System.out.println("________________________");
+    }
+    public Timer getTimer() {
+        return timer;
+    }
+}
